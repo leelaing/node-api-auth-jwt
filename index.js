4b9adaec39574eac0cf47d96eb20bf304bbf3f40
Lee Laing <big2tiny@gmail.com>
2019-07-30 13:36:35 -0400

    2nd commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
+// Connect to db
+mongoose.connect(
+  process.env.DB_CONNECT_STRING,
+  { useNewUrlParser: true },
+  () => {
+    console.log('Connected to db!')
+  }
+)
 // Import routes
 const authRoute = require('./routes/auth')
 
 // Route Middleware
 app.use('/api/user', authRoute)
 
-app.listen(4000, () => console.log('Server now running on port 4000'))
+app.listen(process.env.PORT, () =>
+  console.log(`Server now running on port ${process.env.PORT}`)
+)
