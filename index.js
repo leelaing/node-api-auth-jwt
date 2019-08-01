68e758f51a0e86eaabb9e06df0612a3ea997408b
Lee Laing <big2tiny@gmail.com>
2019-08-01 13:45:39 -0400

    Added cors module

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
+
+app.use(cors)
 
 // Import routes
 const authRoute = require('./routes/auth')
